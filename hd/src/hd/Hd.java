ffacc6da6b20bddd9786efcbf8640bdd12257c42
henrryu <henrryorozco@yahoo.es>
2016-02-02 15:06:52 -0500

    Update Hd.java

--- hd/src/hd/Hd.java
+++ hd/src/hd/Hd.java
@@ -15,7 +15,8 @@ public class Hd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.printl("el es ");
+        
+            }
     
 }
